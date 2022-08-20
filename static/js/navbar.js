875cf557bdba262e05371395368980a2d5452941
soujanya hazra <soujanya0105@gmail.com>
2022-08-20 13:22:52 +0530

    navbar scroll color change added

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,3 +3,10 @@ $('.menu-toggle').click(function () {
 	$(".nav").toggleClass("mobile-nav");
 	$(this).toggleClass("is-active");
 });
+
+$(function () {
+	$(document).scroll(function () {
+		var $nav = $(".navbar-section");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
