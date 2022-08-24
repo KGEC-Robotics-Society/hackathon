419be60f79e930c8d68308265861c5ef9d8999eb
sudip101-arc <maitysudip6524@gmail.com>
2022-08-25 00:27:03 +0530

    problem statements section done, side nav icon design changes

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,25 +1,24 @@
 const hero = document.getElementById("hero");
 const header = document.querySelector(".navbar-section");
 
-$('.menu-toggle').click(function () {
-	$(".nav").toggleClass("mobile-nav");
-	$(this).toggleClass("is-active");
+$(".menu-toggle").click(function () {
+  $(".nav").toggleClass("mobile-nav");
+  $(this).toggleClass("is-active");
+  $(".register-btn-div").toggleClass("mobile-nav-register");
 });
 
-
 const observer = new IntersectionObserver(
-	(entries) => {
-		const ent = entries[0];
-		ent.isIntersecting === false
-			? header.classList.add("sticky")
-			: header.classList.remove("sticky")
-	},
-	{
-
-		root: null,
-		rootMargin: "",
-		threshold: "0"
-
-	});
+  (entries) => {
+    const ent = entries[0];
+    ent.isIntersecting === false
+      ? header.classList.add("sticky")
+      : header.classList.remove("sticky");
+  },
+  {
+    root: null,
+    rootMargin: "",
+    threshold: "0",
+  }
+);
 
 observer.observe(hero);
